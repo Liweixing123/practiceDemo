1b7418185eeb677a54a89dbb763d6e65791e991b
liweixing <2710156303@qq.com>
2023-01-08 18:34:27 +0800

    first commit

---
+++ src/components/HelloWorld.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>
+    <h1>
+      "这是值得纪念的一天"
+    </h1>
+  </div>
+</template>
+
+<script setup lang="ts">
+import { ref } from 'vue'
+import {} from './HelloWorld.vue'
+
+</script>
+
+<style scoped>
+
+</style>
